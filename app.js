77a2056b3fdd7f05edf21e0d8b61804da90b7868
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-24 14:16:52 +0300

    backend <> create signup user route

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
+const user = require("./routes/user.routes");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+
+app.use("/api/v1/users", user);
 
 module.exports = app;
