86472a535fd3654509a2426d77b715cb9fe0e057
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-24 14:57:51 +0300

    backend <> add morgan library

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const user = require("./routes/user.routes");
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
