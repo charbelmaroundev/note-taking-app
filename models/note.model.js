cd6606a1152ed10ad8925d877a869f4a4a89ad35
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-24 17:26:40 +0300

    backend <> add create note model

---
+++ models/note.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please enter a title for note"],
+    maxlength: [10, "A note title must have less or equal then 10 characters"],
+    minlength: [2, "A note title must have more or equal then 2 characters"],
+  },
+
+  content: {
+    type: String,
+    required: [true, "Please enter a content for note!"],
+    maxlength: [
+      100,
+      "A note content must have less or equal then 100 characters",
+    ],
+  },
+
+  tags: {
+    type: [Array],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+
+  updatedAt: {
+    type: Date,
+    select: false,
+  },
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
