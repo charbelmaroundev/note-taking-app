ca9f30239d5ab9c1641cad22d42e7ded31f486a4
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-25 18:15:48 +0300

    backend <> add category model

---
+++ models/category.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name for category"],
+  },
+
+  notes_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    // ref: "Note",
+  },
+
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
