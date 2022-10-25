0ef1da4cc1914231d8805a37c8ccd03fda75244b
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-25 12:00:17 +0300

    backend <> add get notes by user id

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -1,4 +1,5 @@
 const Note = require("../models/note.model");
+const User = require("../models/user.models");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("../utils/appError");
 const APIFeatures = require("../utils/apiFeatures");
@@ -7,6 +8,10 @@ const getAllNotes = catchAsync(async (req, res, next) => {
   const features = new APIFeatures(Note.find(), req.query).sort();
 
   const notes = await features.query;
+
+  if (!notes.length) {
+    return next(new AppError("No notes found!", 404));
+  }
 
   res.status(200).json({
     status: "success",
@@ -21,6 +26,7 @@ const getNote = catchAsync(async (req, res, next) => {
   const { id } = req.params;
 
   const note = await Note.findById(id).select("+createdAt");
+  // console.log(note);
 
   if (!note) {
     return next(new AppError("No note found with that ID", 404));
@@ -34,15 +40,45 @@ const getNote = catchAsync(async (req, res, next) => {
   });
 });
 
+const getNotesByUser = catchAsync(async (req, res, next) => {
+  const { user_id } = req.params;
+
+  const notes = await Note.find({ creator: user_id })
+    .select("-__v")
+    .select("-creator")
+    .select("-updatedAt")
+    .select("-_id");
+
+  if (!notes.length) {
+    console.log("hahah");
+    return next(new AppError("No notes found for this user", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    results: notes.length,
+    data: {
+      notes,
+    },
+  });
+});
+
 const createNote = catchAsync(async (req, res, next) => {
   const { title, content } = req.body;
   const current_id = req.user;
-  // console.log(current_id);
+
   const newNote = await Note.create({
     title,
     content,
-    user_id: current_id,
+    creator: current_id,
   });
+
+  await User.updateOne(
+    { _id: current_id },
+    {
+      $push: { notes: newNote.id },
+    }
+  );
 
   res.status(201).json({
     status: "success",
@@ -78,8 +114,12 @@ const updateNote = catchAsync(async (req, res, next) => {
 
 const deleteNote = catchAsync(async (req, res, next) => {
   const { id } = req.params;
+  const current_id = req.user;
+  // console.log(current_id);
 
   const note = await Note.findByIdAndDelete(id);
+
+  // console.log(note.user_id);
 
   if (!note) {
     return next(new AppError("No Note found with that ID", 404));
@@ -91,4 +131,11 @@ const deleteNote = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { getAllNotes, getNote, createNote, updateNote, deleteNote };
+module.exports = {
+  getAllNotes,
+  getNote,
+  getNotesByUser,
+  createNote,
+  updateNote,
+  deleteNote,
+};
