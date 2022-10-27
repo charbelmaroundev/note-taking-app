f10efec3e156bf32a0f0de8210459583459de2d5
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-27 10:57:25 +0300

    backend <> add filter and sort get categories

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,10 +1,10 @@
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("../utils/appError");
+const APIFeatures = require("../utils/apiFeatures");
+
 const Category = require("../models/category.model");
 const User = require("../models/user.models");
 const Note = require("../models/note.model");
-
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("../utils/appError");
-// const APIFeatures = require("../utils/apiFeatures");
 
 const createCategory = catchAsync(async (req, res, next) => {
   const { name } = req.body;
@@ -16,7 +16,7 @@ const createCategory = catchAsync(async (req, res, next) => {
     return next(new AppError(`${name} is taken`, 404));
   }
 
-  await User.updateMany(
+  await User.updateOne(
     {
       _id: current_id,
     },
@@ -41,9 +41,16 @@ const createCategory = catchAsync(async (req, res, next) => {
 const getCategories = catchAsync(async (req, res, next) => {
   const current_id = req.user;
 
-  const categories = await Category.find({ user_id: current_id }).select(
-    "-notes_id -user_id -__v"
-  );
+  const features = new APIFeatures(
+    Category.find({ user_id: current_id })
+      .select("-notes_id -user_id -__v")
+      .select("-__v -creator -_id -updatedAt"),
+    req.query
+  )
+    .filter()
+    .sort();
+
+  const categories = await features.query;
 
   if (!categories.length) {
     return next(new AppError("No categories found", 404));
@@ -58,14 +65,71 @@ const getCategories = catchAsync(async (req, res, next) => {
   });
 });
 
-const deleteCategories = catchAsync(async (req, res, next) => {
+const updateCategory = catchAsync(async (req, res, next) => {
   const { id } = req.params;
+  const { name } = req.body;
   const current_id = req.user;
+
+  const checkCategory = await Category.find({ _id: id, user_id: current_id });
+
+  if (!checkCategory.length) {
+    return next(new AppError(`Category not found`, 404));
+  }
+
+  await Category.updateMany(
+    {
+      user_id: current_id,
+      _id: id,
+    },
+    {
+      name: name,
+    }
+  );
+
+  await User.updateOne(
+    {
+      _id: current_id,
+    },
+    {
+      $pull: { categories: checkCategory[0].name },
+    }
+  );
+
+  await User.updateOne(
+    {
+      _id: current_id,
+    },
+    {
+      $push: { categories: name },
+    }
+  );
+
+  await Note.updateMany(
+    {
+      creator: current_id,
+      category: checkCategory[0].name,
+    },
+    {
+      category: name,
+    }
+  );
 
   const category = await Category.find({ _id: id, user_id: current_id });
 
-  if (!category.length) {
-    return next(new AppError(`This category is not found`, 404));
+  res.status(200).json({
+    status: "success",
+    data: category,
+  });
+});
+
+const deleteCategory = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const current_id = req.user;
+
+  const checkCategory = await Category.find({ _id: id, user_id: current_id });
+
+  if (!checkCategory.length) {
+    return next(new AppError(`Category not found`, 404));
   }
 
   const notesId = category[0].notes_id;
@@ -98,65 +162,9 @@ const deleteCategories = catchAsync(async (req, res, next) => {
   });
 });
 
-const updateCategories = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const current_id = req.user;
-
-  const checkCategory = await Category.find({ _id: id, user_id: current_id });
-
-  if (!checkCategory.length) {
-    return next(new AppError(`This category is not found`, 404));
-  }
-
-  await Category.updateMany(
-    {
-      user_id: current_id,
-      _id: id,
-    },
-    {
-      name: name,
-    }
-  );
-
-  await User.updateMany(
-    {
-      _id: current_id,
-    },
-    {
-      $pull: { categories: checkCategory[0].name },
-    }
-  );
-
-  await User.updateMany(
-    {
-      _id: current_id,
-    },
-    {
-      $push: { categories: name },
-    }
-  );
-
-  await Note.updateMany(
-    {
-      creator: current_id,
-    },
-    {
-      category: name,
-    }
-  );
-
-  const category = await Category.find({ _id: id, user_id: current_id });
-
-  res.status(200).json({
-    status: "success",
-    data: category,
-  });
-});
-
 module.exports = {
   getCategories,
   createCategory,
-  deleteCategories,
-  updateCategories,
+  deleteCategory,
+  updateCategory,
 };
