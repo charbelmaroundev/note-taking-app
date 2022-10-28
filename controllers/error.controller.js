4aba0729d59361660ec22a9f5bf7480c57fc2613
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-28 18:17:59 +0300

    backend <> add jwt expired error in production

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -23,6 +23,9 @@ const handleValidationErrorDB = (err) => {
 const handleJWTError = () =>
   new AppError("Invalid token. Please log in again!", 401);
 
+const handleJWTExpiredError = () =>
+  new AppError("Your token has expired! Please log in again.", 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -46,5 +49,6 @@ module.exports = (err, req, res, next) => {
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
     if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
   }
 };
