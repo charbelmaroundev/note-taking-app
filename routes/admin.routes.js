6498692792a6d3e69efb4dbbe6e51a1248ab23af
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-28 18:46:37 +0300

    backend <> add admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, login } = require("../controllers/auth.controller");
+const {
+  getAllNotes,
+  getNote,
+  getAllUsers,
+} = require("../controllers/admin.controller");
 const { authMiddleware } = require("../middlewares/auth.middleware");
+const { adminMiddleware } = require("../middlewares/admin.middleware");
 
-router.post("/signup", authMiddleware, signup);
-router.post("/login", authMiddleware, login);
+router.route("/notes").get(authMiddleware, adminMiddleware, getAllNotes);
+
+router.route("/users").get(authMiddleware, adminMiddleware, getAllUsers);
+
+router.route("/notes/:id").get(authMiddleware, adminMiddleware, getNote);
 
 module.exports = router;
