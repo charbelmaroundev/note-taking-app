d1494955e4882f6149468b792576fa660a9a67ea
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-28 18:19:27 +0300

    backend <> add send error production function

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -35,6 +35,21 @@ const sendErrorDev = (err, res) => {
   });
 };
 
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -50,5 +65,7 @@ module.exports = (err, req, res, next) => {
       error = handleValidationErrorDB(error);
     if (error.name === "JsonWebTokenError") error = handleJWTError();
     if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+    sendErrorProd(error, res);
   }
 };
